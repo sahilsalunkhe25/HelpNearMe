c03d3d4a5994b39f78fa81a794684672eab35cb6
sahilsalunkhe25 <sahilsalunkhe.25@gmail.com>
2025-04-10 12:19:26 +0530

    Initial Configuration Files

---
+++ HelpNearMe/src/main/java/com/example/HelpNearMe/HelpNearMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelpNearMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpNearMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpNearMeApplication.class, args);
+	}
+
+}
---
+++ HelpNearMe/src/test/java/com/example/HelpNearMe/HelpNearMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelpNearMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpNearMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
