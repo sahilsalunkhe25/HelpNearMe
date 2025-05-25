fe9c72176473521bddb346ef7e712acba2a551ea
sahilsalunkhe25 <sahilsalunkhe.25@gmail.com>
2025-05-25 20:51:44 +0530

    Added purpose page

--- HelpNearMeUI/helpnearme/src/App.js
+++ HelpNearMeUI/helpnearme/src/App.js
@@ -8,6 +8,8 @@ import AdminDashboardPage from './pages/AdminDashboardPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
+import PurposePage from './pages/PurposePage'; // or wherever you save the component
+
 
 function App() {
   return (
@@ -20,6 +22,7 @@ function App() {
           <Route path="/helper/:id" element={<HelperDetailsPage />} />
           <Route path="/admin" element={<AdminLoginPage />} />
           <Route path="/admin/dashboard" element={<AdminDashboardPage />} />
+          <Route path="/purpose" element={<PurposePage />} />
         </Routes>
         <Footer />
       </Router>
